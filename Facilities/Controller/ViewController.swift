8699800b91f3cd597cc484ccd07ef48444fcce2b
hager <hagergamal346@gmail.com>
2019-02-02 06:01:42 +0200

    add fade animation in scrolling and in reload table view

--- Facilities/Controller/ViewController.swift
+++ Facilities/Controller/ViewController.swift
@@ -26,17 +26,53 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
         serviceTableView.register(UINib(nibName: "ServiceTableViewCell", bundle: nil), forCellReuseIdentifier: "myDataCell")
         self.serviceTableView.dataSource = self
         self.serviceTableView.delegate = self
-        
         NetworkService.shared.getServices(pageSize: 10, pageIndex: 1, departmentNumber: 2) { data , error in
             if data != nil {
                 self.services = (data?.data)!
                 self.activityIndicator.hidesWhenStopped=true
                 self.activityIndicator.stopAnimating()
-                self.serviceTableView.reloadData()
+                self.serviceTableView.reloadData {
+                    self.animateTable()
+                }
+            }
+        }
+    }
+    // MARK:- Prepare For Segue
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let backItem = UIBarButtonItem()
+        backItem.title = ""
+        navigationItem.backBarButtonItem = backItem
+        if segue.identifier == "goToDetails"
+        {
+            if let serviceDetailsViewcontroller = segue.destination as? ServiceDetailsViewController{
+                serviceDetailsViewcontroller.service = self.services?[selectedCell ?? 0]
             }
         }
     }
     
+    func animateTable() {
+        serviceTableView.reloadData()
+        
+        let cells = serviceTableView.visibleCells
+        let tableHeight: CGFloat = serviceTableView.bounds.size.height
+        
+        for i in cells {
+            let cell: UITableViewCell = i as UITableViewCell
+            cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+        }
+        
+        var index = 0
+        
+        for a in cells {
+            let cell: UITableViewCell = a as UITableViewCell
+            UIView.animate(withDuration: 1.5, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
+                cell.transform = CGAffineTransform(translationX: 0, y: 0);
+            }, completion: nil)
+            
+            index += 1
+        }
+    }
     // MARK:- Table Data Source
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -55,23 +91,27 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
         return cell
     }
     
+    // MARK:- Table Delegate
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedCell = indexPath.row
         performSegue(withIdentifier: "goToDetails", sender: self)
         }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let backItem = UIBarButtonItem()
-        backItem.title = ""
-        navigationItem.backBarButtonItem = backItem
-        if segue.identifier == "goToDetails"
-        {
-            if let serviceDetailsViewcontroller = segue.destination as? ServiceDetailsViewController{
-                serviceDetailsViewcontroller.service = self.services?[selectedCell ?? 0]
-            }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.alpha = 0
+        UIView.animate(withDuration: 1.0) {
+            cell.alpha = 1.0
         }
     }
-
+   
 
     
+}
+
+extension UITableView {
+    func reloadData(completion: @escaping ()->()) {
+        UIView.animate(withDuration: 0, animations: { self.reloadData() })
+        { _ in completion() }
+    }
 }
